121129e68e46e929e498cd332ee300cc30b8f1fc
Sarkhan Jafarli <sarkhanjafarli12@gmail.com>
2021-04-29 01:03:23 +0400

    Create Home page gradient bg

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "../styles/Home.css";
+
+export default function Home() {
+  
+  return (
+    <div className="test">
+      <h1>Codenames online</h1>
+    </div>
+  );
+}
