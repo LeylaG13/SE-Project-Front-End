7b64ddac97bc019544f9dd5d4b277424c6e98d1f
Sarkhan Jafarli <sarkhanjafarli12@gmail.com>
2021-05-01 10:33:59 +0400

    Did Homepage and About

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
-import Home from '../pages/Home'
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import Home from "../pages/Home";
+import About from "../pages/About";
 export default class App extends Component {
   render() {
     return (
-      <>
-        <Home />
-      </>
-    )
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,47 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../styles/Home.css";
 
+import spy1 from "../media/sp1.png";
+import spy2 from "../media/sp2.png";
+
 export default function Home() {
-  
   return (
     <div className="test">
-      <h1>Codenames online</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Codenames Online</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-3">
+            <div className="image-box">
+              <img src={spy1} alt="spy one" />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="first row">
+              <div className="col-md-12">
+                <Link to="/createroom" className="button">Create Room</Link>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <Link to="/login" className="button">Log In</Link>
+              </div>
+              <div className="col-md-6">
+                <Link to="/signup" className="button">Sign Up</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="image-box">
+              <img src={spy2} alt="spy two" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
