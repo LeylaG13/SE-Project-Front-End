4e91136ae25389de3ce43d7eb62129b87d8ffd53
Sarkhan Jafarli <sarkhanjafarli12@gmail.com>
2021-05-08 21:45:19 +0400

    Added Profile page + some responsiveness

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Profile from '../pages/Profile';
 export default class App extends Component {
   render() {
     return (
@@ -10,6 +11,7 @@ export default class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
+          <Route path="/profile" component={Profile} />
         </Switch>
       </BrowserRouter>
     );
--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-          <h1>About Codenames</h1>
+          <h1 className="about-header">About Codenames</h1>
         </div>
       </div>
       <div className="row">
