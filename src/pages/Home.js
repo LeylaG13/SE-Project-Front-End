25a630c193b60554e68c0bc02f6346f24fc0a6cd
Sarkhan Jafarli <sarkhanjafarli12@gmail.com>
2021-05-22 14:23:22 +0400

    Modal prototype is created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Menu from "../components/Menu";
+import Modal from '../components/Modal'
+
 import "../styles/Home.css";
 import "../style.css";
 
@@ -8,11 +10,24 @@ import spy1 from "../media/sp1.png";
 import spy2 from "../media/sp2.png";
 
 export default function Home() {
+  const [show, setShow] = useState(false);
+  
+  const showModal = () => {
+    setShow(true);
+  }
+
+  const hideModal = () => {
+    setShow(false);
+    
+  }
   return (
     <div>
       <div className="test">
         <Menu />
-
+        <Modal show={show} handleClose={hideModal} className="modal">
+          <p>Gameroom Link</p>
+          <Link to="/gamepage" className="button">Start the Game</Link>
+        </Modal>
         <div className="container">
           <div className="row firstrow">
             <div className="col-md-12">
@@ -28,9 +43,9 @@ export default function Home() {
             <div className="col-md-6">
               <div className="first row">
                 <div className="col-md-12">
-                  <Link to="/gamepage" className="button">
+                  <button className="button" onClick={showModal}>
                     Create Room
-                  </Link>
+                  </button>
                 </div>
               </div>
               <div className="row">
