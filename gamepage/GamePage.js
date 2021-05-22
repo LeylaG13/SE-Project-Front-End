6b0c3ce3f63d072a1406f870450e947dd6367c25
Leyla Gasimova <77205086+LeylaG13@users.noreply.github.com>
2021-05-23 03:42:30 +0400

    Add files via upload

---
+++ gamepage/GamePage.js
@@ -0,0 +1,510 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./GamePage.css";
+import Card from "./Card";
+import Menu from "../components/Menu";
+import icon1 from "../media/cards/icon1.png";
+import icon2 from "../media/cards/icon2.png";
+
+const allCards = [
+  { id: "1", word: "bus", color: "red", is_open: 0, game: "khsfhk" },
+  { id: "2", word: "code", color: "red", is_open: 0, game: "khsfhk" },
+  { id: "3", word: "success", color: "red", is_open: 0, game: "khsfhk" },
+  { id: "4", word: "water", color: "red", is_open: 0, game: "khsfhk" },
+  { id: "5", word: "book", color: "red", is_open: 0, game: "khsfhk" },
+  { id: "6", word: "cup", color: "red", is_open: 0, game: "khsfhk" },
+  { id: "7", word: "light", color: "red", is_open: 0, game: "khsfhk" },
+  { id: "8", word: "phone", color: "red", is_open: 0, game: "khsfhk" },
+  { id: "9", word: "wall", color: "blue", is_open: 0, game: "khsfhk" },
+  { id: "10", word: "sticker", color: "blue", is_open: 0, game: "khsfhk" },
+  { id: "11", word: "cold", color: "blue", is_open: 0, game: "khsfhk" },
+  { id: "12", word: "hot", color: "blue", is_open: 0, game: "khsfhk" },
+  { id: "13", word: "paper", color: "blue", is_open: 0, game: "khsfhk" },
+  { id: "14", word: "pen", color: "blue", is_open: 0, game: "khsfhk" },
+  { id: "15", word: "color", color: "blue", is_open: 0, game: "khsfhk" },
+  { id: "16", word: "bottle", color: "blue", is_open: 0, game: "khsfhk" },
+  { id: "17", word: "talk", color: "blue", is_open: 0, game: "khsfhk" },
+  { id: "18", word: "hair", color: "grey", is_open: 0, game: "khsfhk" },
+  { id: "19", word: "computer", color: "grey", is_open: 0, game: "khsfhk" },
+  { id: "20", word: "guitar", color: "grey", is_open: 0, game: "khsfhk" },
+  { id: "21", word: "singing", color: "grey", is_open: 0, game: "khsfhk" },
+  { id: "22", word: "icecream", color: "grey", is_open: 0, game: "khsfhk" },
+  { id: "23", word: "painting", color: "grey", is_open: 0, game: "khsfhk" },
+  { id: "24", word: "notebook", color: "grey", is_open: 0, game: "khsfhk" },
+  { id: "25", word: "lipstick", color: "black", is_open: 0, game: "khsfhk" },
+];
+
+const numbers = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23, 24,
+];
+
+var glob = 0;
+const GamePage = () => {
+  const shuffle = (array) => {
+    array.sort(() => Math.random() - 0.2);
+  };
+
+  if (glob === 0) {
+    shuffle(allCards);
+    shuffle(allCards);
+    shuffle(allCards);
+    shuffle(numbers);
+    shuffle(numbers);
+    shuffle(numbers);
+    glob++;
+  }
+  // const [player, setPlayer] = useState("");
+  const [pointsRed, setPointsRed] = useState(0);
+  const [pointsBlue, setPointsBlue] = useState(0);
+  const [chosenCard, setChosenCard] = useState({});
+  const [team, setTeam] = useState("");
+  const [disabled, setDisabled] = useState("");
+  const [numBlueSpy, setNumBlueSpy] = useState(0);
+  const [numBlueOperative, setNumBlueOperatives] = useState(0);
+  const [numRedSpy, setNumRedSpy] = useState(0);
+  const [numRedOperative, setNumberRedOperative] = useState(0);
+  const [player, setPlayer] = useState("");
+  const [whoseTurn, setWhoseTurn] = useState(0);
+  const [messages, setMessages] = useState([
+    {
+      id: 1,
+      msg: "Hello",
+      team: 0, // blue
+    },
+    {
+      id: 2,
+      msg: "You are stupid, go home",
+      team: 1, //red
+    },
+  ]);
+
+  const [hint, setHint] = useState("");
+  const [moves, setMoves] = useState(0);
+
+  const [lastID, setLastID] = useState(2);
+
+  // useEffect(() => {
+  //   axios.get("http://127.0.0.1:8000/api/generate").then((response) => {
+  //     console.log(response.data);
+  //   });
+  // }, []);
+
+  // const [token, setToken] = useState("");
+  // const [allCards, setAllCards] = useState([]);
+
+  // var user = {};
+  // useEffect(() => {
+  //   axios
+  //     .post("http://127.0.0.1:8000/api/signin", {
+  //       email: "hfh@email.com",
+  //       password: "123",
+  //     })
+  //     .then(
+  //       (response) => {
+  //         setToken(response.data.token);
+  //         console.log(response.data.token);
+  //       },
+  //       (error) => {
+  //         console.log(error);
+  //       }
+  //     );
+
+  //   axios
+  //     .get("http://127.0.0.1:8000/api/generate", {
+  //       headers: {
+  //         Authorization: `token ${token}`,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data.user);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    if (Object.keys(chosenCard).length !== 0) {
+      if (chosenCard.color === "black") {
+        if (team === "blue") {
+          setPointsBlue(pointsBlue - 100);
+          setPointsRed(pointsRed + 450);
+        } else {
+          setPointsRed(pointsRed - 100);
+          setPointsBlue(pointsBlue + 450);
+        }
+      } else if (chosenCard.color === team) {
+        if (team === "blue") {
+          console.log("I am blue");
+          setPointsBlue(pointsBlue + 50);
+        } else {
+          console.log("I am red");
+
+          setPointsRed(pointsRed + 50);
+        }
+      } else if (chosenCard.color === "grey") {
+      } else {
+        if (team === "blue") {
+          console.log("I am blue");
+
+          setPointsBlue(pointsBlue - 25);
+          setPointsRed(pointsRed + 25);
+        } else {
+          console.log("I am red");
+
+          setPointsRed(pointsRed - 25);
+          setPointsBlue(pointsBlue + 25);
+        }
+      }
+    }
+  }, [chosenCard]);
+
+  console.log(chosenCard);
+  const handleMoves = (e) => {
+    setMoves(Number(e.target.value));
+  };
+
+  const handleHint = (e) => {
+    setHint(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessages([...messages, { id: lastID + 1, msg: hint, team: whoseTurn }]);
+    setWhoseTurn(!whoseTurn);
+    setLastID(lastID + 1);
+  };
+
+  const onClickTeam = (playertype, color) => {
+    setPlayer(playertype);
+    setTeam(color);
+    setDisabled("disabled");
+    if (playertype === "spymaster" && color === "blue") {
+      setNumBlueSpy(numBlueSpy + 1);
+      // numBlueSpy+=1;
+    } else if (playertype === "spymaster" && color === "red") {
+      setNumRedSpy(numRedSpy + 1);
+    } else if (playertype === "operative" && color === "red") {
+      setNumberRedOperative(numRedOperative + 1);
+    } else if (playertype === "operative" && color === "blue") {
+      setNumBlueOperatives(numBlueOperative + 1);
+    }
+
+    // console.log(player, team);
+  };
+
+  return (
+    <div id="gamepage">
+      <Menu />
+      <h1> Room #1</h1>
+      <button
+        class={`redbutton ui ${
+          player === "spymaster" && team === "blue" ? "" : `inverted`
+        } small blue button  ${disabled}`}
+        onClick={(e) => onClickTeam("spymaster", "blue")}
+      >
+        Join as spymaster
+      </button>
+      <button
+        class={`redbutton  ui ${
+          player === "operative" && team === "blue" ? "" : `inverted`
+        } small blue button ${disabled}`}
+        id="rightbutton"
+        onClick={(e) => onClickTeam("operative", "blue")}
+      >
+        Join as operative
+      </button>
+      <button
+        class={`bluebutton  ui ${
+          player === "spymaster" && team === "red" ? "" : `inverted`
+        } small pink button ${disabled}`}
+        onClick={(e) => onClickTeam("spymaster", "red")}
+      >
+        Join as spymaster
+      </button>
+      <button
+        class={`bluebutton  ui ${
+          player === "operative" && team === "red" ? "" : `inverted`
+        } small pink button ${disabled}`}
+        onClick={(e) => onClickTeam("operative", "red")}
+      >
+        Join as operative
+      </button>
+
+      <div className="ui grid">
+        <div className="three wide column">
+          <div className="icon-image blue">
+            <img src={icon1} alt="icon1" />
+          </div>
+          <div className="game-info one">
+            <h4>Team Blue</h4>
+            <p>Words guessed: </p>
+            <p>Points earned: {pointsBlue} </p>
+            <p>Operatives: {numBlueOperative}</p>
+            <p>Spymasters: {numBlueSpy}</p>
+          </div>
+        </div>
+
+        {/* First Column */}
+        <div className="two wide column">
+          <Card
+            word={allCards[0].word}
+            color={allCards[0].color}
+            number={numbers[0]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[1].word}
+            color={allCards[1].color}
+            number={numbers[1]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[2].word}
+            color={allCards[2].color}
+            number={numbers[2]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[3].word}
+            color={allCards[3].color}
+            number={numbers[3]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[4].word}
+            color={allCards[4].color}
+            number={numbers[4]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+        </div>
+
+        {/* Second Column */}
+        <div className="two wide column">
+          <Card
+            word={allCards[5].word}
+            color={allCards[5].color}
+            number={numbers[5]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[6].word}
+            color={allCards[6].color}
+            number={numbers[6]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[7].word}
+            color={allCards[7].color}
+            number={numbers[7]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[8].word}
+            color={allCards[8].color}
+            number={numbers[8]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[9].word}
+            color={allCards[9].color}
+            number={numbers[9]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+        </div>
+
+        {/* Third Column */}
+        <div className="two wide column">
+          <Card
+            word={allCards[10].word}
+            color={allCards[10].color}
+            number={numbers[10]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[11].word}
+            color={allCards[11].color}
+            number={numbers[11]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[12].word}
+            color={allCards[12].color}
+            number={numbers[12]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[13].word}
+            color={allCards[13].color}
+            number={numbers[13]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[14].word}
+            color={allCards[14].color}
+            number={numbers[14]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+        </div>
+
+        {/* Fourth Column */}
+        <div className="two wide column">
+          <Card
+            word={allCards[15].word}
+            color={allCards[15].color}
+            number={numbers[15]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[16].word}
+            color={allCards[16].color}
+            number={numbers[16]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[17].word}
+            color={allCards[17].color}
+            number={numbers[17]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[18].word}
+            color={allCards[18].color}
+            number={numbers[18]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[19].word}
+            color={allCards[19].color}
+            number={numbers[19]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+        </div>
+
+        {/* Fifth Column */}
+
+        <div className="two wide column">
+          <Card
+            word={allCards[20].word}
+            color={allCards[20].color}
+            number={numbers[20]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[21].word}
+            color={allCards[21].color}
+            number={numbers[21]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[22].word}
+            color={allCards[22].color}
+            number={numbers[22]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[23].word}
+            color={allCards[23].color}
+            number={numbers[23]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+          <Card
+            word={allCards[24].word}
+            color={allCards[24].color}
+            number={numbers[24]}
+            setChosenCard={setChosenCard}
+            player={player}
+          />
+        </div>
+
+        <div className="three wide column">
+          <div className="icon-image red">
+            <img src={icon2} alt="icon2" />
+          </div>
+          <div className="game-info two">
+            <h4>Team Red</h4>
+            <p>Words guessed: </p>
+            <p>Points earned: {pointsRed} </p>
+            <p>Operatives: {numRedOperative}</p>
+            <p>Spymasters: {numRedSpy}</p>
+          </div>
+          <div className="chatbox">
+            {messages.map((message) => {
+              return (
+                <div
+                  className={`message ${message.team ? "red" : "blue"}`}
+                  key={message.id}
+                >
+                  {message.msg}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+
+      {/* <div>
+        <label for="hint" class="preg">
+          Hint:
+        </label>
+        <input type="text" id="idhint" name="hint" />
+      </div> */}
+
+      <div className="hintbox">
+        <form className="hintform" onSubmit={handleSubmit}>
+          <div>
+            <label for="hint" class="preg">
+              Hint:
+            </label>
+            <input
+              className="hint"
+              value={hint}
+              type="text"
+              id="idhint"
+              name="hint"
+              onChange={handleHint}
+            />
+            <label for="moves" class="preg">
+              Moves:{" "}
+            </label>
+            <input
+              className="moves"
+              value={moves}
+              type="number"
+              name="moves"
+              onChange={handleMoves}
+            />
+          </div>
+          <button class="ui inverted white button large">Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default GamePage;
